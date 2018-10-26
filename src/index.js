1437f658cfa6a8e8962024f12c41452e612c739c
armis <musteikis.arminas@gmail.com>
2018-10-26 19:21:10 +0300

    pr3

--- src/index.js
+++ src/index.js
@@ -117,7 +117,7 @@ class Game extends React.Component {
 
         const status = winner
             ? `Winner: ${winner}`
-            : `Next player1: ${this.state.xIsNext ? "X" : "O"}`;
+            : `Next player: ${this.state.xIsNext ? "X" : "1O"}`;
 
         return (
             <div className="game">
